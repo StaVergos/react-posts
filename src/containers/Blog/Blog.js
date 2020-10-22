337e5e8ac6899b2eb79bd804ff08be4ffc968187
Stavros Vergos <stavrospvergos@gmail.com>
2020-10-23 00:35:18 +0300

    setting up routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import Posts from "./Posts/Posts";
 
@@ -18,7 +19,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/>
             </div>
         );
     }
